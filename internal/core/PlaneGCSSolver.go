7b31b58da08bf3a69004a6beb596c056ed9bb4bc
Marcus Wu <marcus.wu@passportinc.com>
2021-08-14 08:52:01 -0400

    planegcs planning

--- internal/core/PlaneGCSSolver.go
+++ internal/core/PlaneGCSSolver.go
@@ -2,6 +2,26 @@ package core
 
 import "libmakercad/third_party/planegcs"
 
+/*
+   PlaneGCSSolver needs to track fixed geometry vs unfixed geometry so we can declare a list of unknown geometry to GCS
+	GCSsys.invalidatedDiagnosis();
+	GCSsys.declareUnknowns(Parameters);
+	GCSsys.declareDrivenParams(DrivenParameters);
+    GCSsys.initSolution(defaultSolverRedundant);
+
+	For diagnosis:
+	GCSsys.getConflicting(Conflicting);
+	GCSsys.getRedundant(Redundant);
+	GCSsys.getPartiallyRedundant (PartiallyRedundant);
+	GCSsys.getDependentParams(pDependentParametersList);
+	GCSsys.dofsNumber();
+
+	Solve:
+	GCSsys.solve(isFine, GCS::DogLeg or BFGS or LevenbergMarquardt)
+	GCSsys.applySolution();
+
+	If not solved, fall back to other solvers...
+*/
 type PlaneGCSSolver struct {
 	system planegcs.System
 }
